a3445401eeef1530e18fa4950bcc5d8213a89ab9
ouaelesi <ouael2019esi@gmail.com>
2022-10-02 08:20:31 +0100

    final commit nchalah

--- components/StepOne/BirthdayCard.jsx
+++ components/StepOne/BirthdayCard.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect } from "react";
 
 const BirthdayCard = ({ image, bg }) => {
-  useEffect(() => {
-    alert(bg);
-  });
   return (
     <div className="w-64 relative">
       <img src={image} className="w-64" />
